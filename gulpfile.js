b10b2984d3fd976f2b48a23bbc4abe7912a64f40
Raoul <raoul.ufa@yandex.ru>
2018-02-05 23:52:54 +0500

    comments was deleted. task fixed

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,14 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var less = require('gulp-less');
-// var sass = require('gulp-sass');
 
 gulp.task('server', ['styles'], function() {
     browserSync.init({
     	server: { baseDir: './app/'}
     });
     gulp.watch('./app/**/*.html').on('change', browserSync.reload);
-    gulp.watch('./app/less/**/*.less', ['less']);
-    // gulp.watch('./app/sass/**/*.scss', ['sass']);
+    gulp.watch('./app/less/**/*.less', ['styles']);
+   
 });
 
 gulp.task('styles', function() {
@@ -18,12 +17,5 @@ gulp.task('styles', function() {
     .pipe(gulp.dest('./app/css'))
     .pipe(browserSync.stream());
 });
-
-// gulp.task('sass', function() {
-//     return gulp.src('./app/sass/**/*.scss')
-//     .pipe(sass())
-//     .pipe(gulp.dest('./app/css'))
-//     .pipe(browserSync.stream());
-// });
 
 gulp.task('default', ['server']);
